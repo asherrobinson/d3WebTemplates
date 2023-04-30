3203d297ab4b55e820c16b9ec9c882999bf2b9ee
asher <robinsonaw@gmail.com>
2023-04-30 18:12:39 -0400

    ProjectorStatus : Initial Implementation

---
+++ ProjectorStatus/defaults.js
@@ -0,0 +1,18 @@
+/**
+ * These properties define the display of the interface.
+ * They're not exposed in d3, currently.
+ */
+
+
+const prjStatusModuleDefaults = {
+
+    // Default Colors - use any valid CSS color syntax: 'name', '#00AA33', 'rgba(128, 255, 32, 0.5)', etc.
+    textColor       : 'white',
+    barOutlineColor : 'white',
+    barColor        : '#cccccc',
+
+    // Text Styles
+    fontFamily : 'Futura,sans-serif',
+    fontSize   : '2em',
+
+};
