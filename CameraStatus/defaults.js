9b2c54ab7ab45716720c0fd77a868c25f2c11c12
asher <robinsonaw@gmail.com>
2023-04-30 18:13:04 -0400

    CameraStatus : Initial Implementation. Not Impl: Char/Desc fields lookup

---
+++ CameraStatus/defaults.js
@@ -0,0 +1,27 @@
+/**
+ * These properties define the display of the interface.
+ * The camNameN properties can be accessed in d3
+ * The rest of these are not currently exposed.
+ */
+
+
+const camStatusModuleDefaults = {
+
+    // Default Colors - use any valid CSS color syntax: 'name', '#00AA33', 'rgba(128, 255, 32, 0.5)', etc.
+    textColor       : 'white',
+
+    // Text Styles
+    fontFamily : 'Futura,sans-serif',
+    fontSize   : '2em',
+
+    // Camera Names
+    camName1 : 'CAM111 (Vid Stg)',
+    camName2 : 'CAM112 (Balc Stg)',
+    camName3 : 'CAM113 (Under USR)',
+    camName4 : 'CAM114 (Under USL)',
+    camName5 : 'CAM115 (Under USL)',
+    camName6 : 'CAM116 (Under USL)',
+    camName7 : 'CAM117 (Under USL)',
+    camName8 : 'CAM118 (Under USL)',
+
+};
